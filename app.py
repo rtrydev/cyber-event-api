87e5dff53f1b0f37de3f56d10513ea2608a039ac
rtrydev <r.kulka44@gmail.com>
2022-12-10 21:02:07 +0100

    Add cors

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from threading import Thread
-
 from flask import Flask
+from flask_cors import CORS
 from flask_injector import FlaskInjector
-from injector import singleton
-
 from extensions.dependency_injection import configure
 from extensions.routes_extension import register_routes
 
 app = Flask(__name__)
+CORS(app)
 
 register_routes(app)
 di_container = FlaskInjector(app=app, modules=[configure])
